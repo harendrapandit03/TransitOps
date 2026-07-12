0673f9e933023769e8784f663754f91a8e6935f1
Deewakar <wknows893@gmail.com>
2026-07-12 16:20:32 +0530

    added middleware CORS

--- app/main.py
+++ app/main.py
@@ -8,9 +8,21 @@ from app.routers import reports
 from app.routers import trips
 from app.routers import maintenance
 from app.routers import fuels
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
